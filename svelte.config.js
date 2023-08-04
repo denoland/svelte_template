8ca7c036cbf3b48e9c4a8720da37bcc99ca85bd9
denobot <33910674+denobot@users.noreply.github.com>
2023-08-04 20:31:16 +0900

    init

---
+++ svelte.config.js
@@ -0,0 +1,13 @@
+import adapter from 'sveltekit-adapter-deno';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	preprocess: vitePreprocess(),
+
+	kit: {
+		adapter: adapter()
+	}
+};
+
+export default config;
